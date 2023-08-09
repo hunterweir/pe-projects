c0e61d61b8b38cda12b28691a2f5e9b69700641b
hunterweir <hunterweir@gmail.com>
2023-08-09 09:46:00 -0500

    layout theme project early attempt

---
+++ layout-theme-challenge/modules/graphic-diptych/template.php
@@ -0,0 +1,14 @@
+
+<graphic-diptych>
+
+	<picture>
+		<img src='https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=900&t=st=1691535077~exp=1691535677~hmac=4a89a8377ec351c7a84ca5b0e4858212d5445fec6075d0f0ca44528010e81af4'>
+	</picture>
+	
+	<text-content>
+		<h1 class='loud-voice'>This is a graphic diptych</h1>
+
+		<p>What a fun thing that we have here</p>
+	</text-content>
+
+</graphic-diptych>
