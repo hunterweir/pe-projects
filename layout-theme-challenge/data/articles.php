99ce6f6e815d38867a242f67ec6aa78353293f1b
hunterweir <hunterweir@gmail.com>
2023-08-10 21:59:40 -0500

    Updates to layout theme challenge

--- layout-theme-challenge/data/articles.php
+++ layout-theme-challenge/data/articles.php
@@ -4,31 +4,31 @@ $articles = [
 	[
 		"heading" => "Article or product",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "pigs.png",
 	],
 	[
 		"heading" => "Destination or option",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "smile.png",
 	],
 	[
 		"heading" => "Story or something",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "people.png",
 	],
 	[
 		"heading" => "Banana tandori",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "hay.png",
 	],
 	[
 		"heading" => "Bar B Q",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "baler.png",
 	],
 	[
 		"heading" => "Marsala",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "crowd.png",
 	],
 ];
--- layout-theme-challenge/modules/articles-intro/template.php
+++ layout-theme-challenge/modules/articles-intro/template.php
@@ -2,11 +2,11 @@
 <?php include ('data/articles.php'); ?>
 
 <articles-intro>
-	<header>
+	<heading>
 		<h2 class='attention-voice'>Heading for this section here</h2>
 
 		<p>A short introduction explaining why these articles are here to help you understand if you want to read them.</p>
-	</header>
+	</heading>
 
 	<article-grid>
 		<ul class='article-list'>
