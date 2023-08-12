fde397f7ebb10cc5109fc455ebba48be2feb2f1a
hunterweir <hunterweir@gmail.com>
2023-08-11 23:09:13 -0500

    layout theme challenge back to basics edits

--- layout-theme-challenge/modules/graphic-diptych/template.php
+++ layout-theme-challenge/modules/graphic-diptych/template.php
@@ -1,6 +1,10 @@
 
 <graphic-diptych>
 
+	<picture>
+		<img src="https://civileats.com/wp-content/uploads/2019/09/190905-regenerative-agriculture-herbicides-pesticides-no-til-farming-healthy-soil-roots-nrcs-oregon-2.jpg" alt='$todo'>
+	</picture>
+	
 	<text-content>
 		<h1 class='loud-voice'>This is a graphic diptych</h1>
 
