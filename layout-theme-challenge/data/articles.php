538d89d66e2c468db711070022ebe73c9049858a
hunterweir <hunterweir@gmail.com>
2023-08-21 11:28:30 -0500

    Revive theme challenge with bullets

---
+++ layout-theme-challenge/data/articles.php
@@ -0,0 +1,34 @@
+<?php
+
+$articles = [
+	[
+		"heading" => "Article or product",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "pigs.png",
+	],
+	[
+		"heading" => "Destination or option",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "smile.png",
+	],
+	[
+		"heading" => "Story or something",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "people.png",
+	],
+	[
+		"heading" => "Banana tandori",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "hay.png",
+	],
+	[
+		"heading" => "Bar B Q",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "baler.png",
+	],
+	[
+		"heading" => "Marsala",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "crowd.png",
+	],
+];
