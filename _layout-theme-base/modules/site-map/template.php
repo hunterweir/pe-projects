036df732ba3c8d7f5ffb2a395ee70686084bbefc
hunterweir <hunterweir@gmail.com>
2023-08-21 11:22:32 -0500

    Save theme challenge base

---
+++ _layout-theme-base/modules/site-map/template.php
@@ -0,0 +1,41 @@
+
+<site-map>
+
+
+	<nav class='site-menu'>
+		<ul>
+			<li>
+				<a href='#'>
+					<?php include('modules/logo.php'); ?>
+				</a>
+			</li>
+
+			<li>
+				<a href="goals.php">Goals</a>
+			</li>
+
+			<li>
+				<a href="resume/index.php">Resume</a>
+			</li>
+
+			<li>
+				<a href="projects.php">Projects</a>
+			</li>
+		</ul>
+	</nav>
+
+	<nav class='x-menu'>
+		<h3 class='strong-voice'>Menu heading here</h3>
+
+		<ul>
+			<li>
+				<a href="responsive-layout-garden.php">Layout Garden</a>
+			</li>
+
+			<li> 
+				<a href="archive.php">Archive</a>
+			</li>
+		</ul>
+	</nav>
+
+</site-map>
